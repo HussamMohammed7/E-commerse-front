6ede690ec700daafcd97e52eb93fd0e584eb68ea
HussamMohammed7 <h.m.sharmani@gmail.com>
2023-12-26 15:39:38 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  server: {
+    proxy: {
+      "/api": {
+        ws: true,
+        changeOrigin: true,
+        target: "https://sda-onsite-mern-backend-project-8e6dua5v9-hussam-s-projects.vercel.app"
+      }
+    },
+  },
   
   plugins: [react()]
-
-
-  
- 
 })
